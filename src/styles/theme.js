9ebb5ebc4be7134309a2efd597704c4fcdf9f356
Joshua Crass <joshua.crass@gmail.com>
2024-01-24 10:07:18 -0800

    updated colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,39 +1,40 @@
 const transpiledTheme = {
   light: {
-    primaryColor: '#25B0D3', // Updated primary color
-    secondaryColor: '#F0912C', // Updated secondary color
+    primaryColor: '#25B0D3', // Blue
+    secondaryColor: '#FF9215', // Orange
     background: '#f5f5f5',
-    text: '#333333',
-    accentColor: '#64DDBB',
-    neutral: '#c0cbd4',
-    highlight: '#FFD700',
+    text: '#090909', // Almost Black
+    accentColor: '#034C70', // Dark Blue
+    neutral: '#AAAAAA', // Gray
+    highlight: '#99D0E9', // Light Blue
     white: '#f5f5f5',
-    black: '#000000',
+    black: '#090909',
     gray: '#afafaf',
     transparent: 'transparent',
     separator: '#eaeaea',
-    knobColor: '#2EBFD4',
+    knobColor: '#25B0D3', // Blue
     barColor: '#f5f5f5',
-    pink: '#FF6B6B', // Pink color
-    green: '#64DDBB', // Green color
-    primaryButton: '#0054AA',
+    pink: '#FF6B6B', // Retained Pink color
+    green: '#64DDBB', // Retained Green color
+    primaryButton: '#034C70', // Dark Blue as primary button
   },
   dark: {
-    primaryColor: '#25B0D3', // Updated primary color
-    secondaryColor: '#F0912C', // Updated secondary color
-    background: '#090909',
+    primaryColor: '#25B0D3', // Blue
+    secondaryColor: '#FF9215', // Orange
+    background: '#090909', // Almost Black
     text: '#f5f5f5',
-    accentColor: '#FF910F',
+    accentColor: '#FF9215', // Orange for accent
     neutral: '#757575',
-    highlight: '#FFD700',
+    highlight: '#99D0E9', // Light Blue
     white: '#f5f5f5',
-    black: '#000000',
+    black: '#090909',
     gray: '#afafaf',
     transparent: 'transparent',
-    knobColor: '#2EBFD4',
-    barColor: '#f5f5f5',
-    pink: '#FF6B6B', // Pink color
-    green: '#64DDBB', // Green color
+    knobColor: '#25B0D3', // Blue
+    barColor: '#090909', // Almost Black for contrast
+    pink: '#FF6B6B', // Retained Pink color
+    green: '#64DDBB', // Retained Green color
+    primaryButton: '#034C70', // Dark Blue as primary button
   },
 };
 
