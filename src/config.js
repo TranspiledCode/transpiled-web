38e33ed3f13178f3a6fdf45efac0df37afb8a4bf
Joshua Crass <joshua.crass@gmail.com>
2024-01-25 15:15:03 -0800

    updated videos

--- src/config.js
+++ src/config.js
@@ -8,20 +8,6 @@ const videos = [
     description:
       'Step-by-step guide to get nvm and Fish Shell to play nicely on your Mac.',
   },
-  {
-    id: 'ymCR4tIWZu8',
-    title: 'NVM on Mac for Fish Shell',
-    author: 'Joshua Crass',
-    description:
-      'Step-by-step guide to get nvm and Fish Shell to play nicely on your Mac.',
-  },
-  {
-    id: 'ymCR4tIWZu8',
-    title: 'NVM on Mac for Fish Shell',
-    author: 'Joshua Crass',
-    description:
-      'Step-by-step guide to get nvm and Fish Shell to play nicely on your Mac.',
-  },
 ];
 
 export { videos, companyName };
