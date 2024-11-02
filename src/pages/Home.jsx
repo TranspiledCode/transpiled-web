599ff30320d7dc3180e32e651fd7a9af8a5dfb6c
Dustin Reyes <Dustin@transpiled.com>
2024-11-01 20:00:03 -0600

    updated home page and header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 // src/App.js
 import Hero from 'components/Hero';
 import Footer from 'components/Footer';
+import Header from 'components/Header';
 
 const Home = () => {
   return (
     <>
+      <Header />
       <Hero />
       <Footer />
     </>
