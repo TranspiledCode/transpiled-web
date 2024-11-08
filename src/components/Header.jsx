8878c9972963c81fbaa9593f142c772f1d7ab132
Dustin Reyes <Dustin@transpiled.com>
2024-11-07 20:36:21 -0700

    updated home page and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,48 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { FontAwesomeIcon } from '@fontawesome/react-fontawesome';
 
 const HeaderWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  color: white;
-  font-family: 'poppins', sans-serif;
-  border: 1px solid #214eea;
-  background: #214eea;
-  height: 84px;
-  padding-bottom: 100px;
+    position: fixed;
+    display: flex;
+    justify-content: space-between;
+    color: white;
+    font-family: 'poppins', sans-serif;
+    border: 1px solid #214eea;
+    background: #214eea;
+    padding: 1rem 2rem;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    z-index: 2;
+    width: 100%;
+    
+  }
 `;
 
 const StyledLogo = styled.div`
   font-weight: Bold;
   font-size: 30px;
-  padding-left: 20px;
 `;
 
 const StyledNav = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-  font-size: 12px;
-  padding-top: 35px;
-  masrgin: 0;
+  display: none;
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    font-size: 12px;
+    margin: 0;
+  }
 `;
+
+const StyledMenu = styled.div``;
 
 const Header = () => {
   return (
     <HeaderWrapper>
       <StyledLogo>Transpiled</StyledLogo>
       <StyledNav>Services</StyledNav>
-      <StyledNav>Testimonials</StyledNav>
-      <StyledNav>Contact</StyledNav>
-      <StyledNav>About</StyledNav>
     </HeaderWrapper>
   );
 };
