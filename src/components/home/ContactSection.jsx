1d8ecd88ac5d3ef6e16ee3e513923cb90a8d51b9
Tyron Shi <tyronshi@gmail.com>
2024-11-09 10:23:10 -0500

    Section raw base setup completed

--- src/components/home/ContactSection.jsx
+++ src/components/home/ContactSection.jsx
@@ -1,5 +1,20 @@
+import styled from '@emotion/styled';
+import ContactFormComponent from '../ContactForm';
+
+const SectionTitle = styled.div``;
+const Title = styled.h2``;
+const Subtitle = styled.p``;
+
 const ContactSection = () => {
-  return <></>;
+  return (
+    <>
+      <SectionTitle>
+        <Title>CONTACT US</Title>
+        <Subtitle>Lets Build Your Digital Solution Together</Subtitle>
+      </SectionTitle>
+      <ContactFormComponent />
+    </>
+  );
 };
 
 export default ContactSection;
