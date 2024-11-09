bb2ebbfa15ef9a8c953e7afdf484456b681d5926
Tyron Shi <tyronshi@gmail.com>
2024-11-09 10:16:10 -0500

    created dedicated ContactSection component for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@
 import Hero from 'components/Hero';
 import Footer from 'components/Footer';
 import SampleHeader from 'components/SampleHeader';
+import ContactSection from '../components/home/ContactSection';
 
 const Home = () => {
   return (
     <>
       <SampleHeader />
       <Hero />
+      <ContactSection />
       <Footer />
     </>
   );
