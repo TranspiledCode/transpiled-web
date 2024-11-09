ef2fd9f3cc6e42db9107c3a47da5341bdfd98c09
Tyron Shi <tyronshi@gmail.com>
2024-11-09 10:37:00 -0500

    Basic section styling added

--- src/components/home/ContactSection.jsx
+++ src/components/home/ContactSection.jsx
@@ -1,19 +1,42 @@
 import styled from '@emotion/styled';
 import ContactFormComponent from '../ContactForm';
 
-const SectionTitle = styled.div``;
-const Title = styled.h2``;
-const Subtitle = styled.p``;
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-image: linear-gradient(
+    ${({ theme }) => theme.colors.darkBlue},
+    #dd53ff
+  );
+`;
+const SectionInfo = styled.div`
+  width: 100%;
+  color: ${({ theme }) => theme.colors.white};
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+`;
+const Title = styled.h2`
+  font-size: 6.4rem;
+  line-height: 6.4rem;
+`;
+const Subtitle = styled.p`
+  font-size: 2.4rem;
+  line-height: 2.4rem;
+`;
 
 const ContactSection = () => {
   return (
-    <>
-      <SectionTitle>
+    <Section>
+      <SectionInfo>
         <Title>CONTACT US</Title>
         <Subtitle>Lets Build Your Digital Solution Together</Subtitle>
-      </SectionTitle>
+      </SectionInfo>
       <ContactFormComponent />
-    </>
+    </Section>
   );
 };
 
