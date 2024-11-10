49af9b0f1df73a8c6f954a2f3efa84deb2ea83e2
Dustin Reyes <Dustin@transpiled.com>
2024-11-09 18:31:29 -0700

    Created Footerbar component

---
+++ src/components/Footerbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const background = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid black;
+  background-color: black;
+  height: 70px;
+  width: auto;
+`;
+const copyRight = styled.div`
+  font-family: Manrope;
+  font-size: 20px;
+`;
+const scrollTop = styled.div`
+  font-family: Manrope;
+  font-size: 20px;
+`;
+
+<background>
+  <copyRight>&#169; Copyright 2024</copyRight>
+  <scrollTop>Scroll to Top &#8593;</scrollTop>
+</background>;
+
+export default Footerbar;
