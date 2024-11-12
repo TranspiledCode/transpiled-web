33b79ddb03ac7dac5bd77634a52c7c0e80b0551b
Tyron Shi <tyronshi@gmail.com>
2024-11-12 12:10:20 -0500

    Setup card component structure

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div``;
+const Image = styled.div``;
+const Heading = styled.h3``;
+const Description = styled.p``;
+
+const CardHeading = '';
+const CardDescription = '';
+
+const Card = () => {
+  return (
+    <Container>
+      <Image></Image>
+      <Heading>{CardHeading}</Heading>
+      <Description>{CardDescription}</Description>
+    </Container>
+  );
+};
+export default Card;
