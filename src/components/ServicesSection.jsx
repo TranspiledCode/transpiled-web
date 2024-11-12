ba32c04d300e9e48785611db68fa177c9de2d1ac
Tyron Shi <tyronshi@gmail.com>
2024-11-12 18:59:45 -0500

    Setup services section to have cards mapped

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,12 +1,32 @@
 import styled from '@emotion/styled';
 import Card from './Card';
+import config from '../config/home';
 
-const Section = styled.div``;
+const Section = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 2rem;
+  padding: 2rem;
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    grid-template-columns: repeat(3, 1fr);
+    padding: 6rem;
+  }
+`;
 
 const ServicesSection = () => {
+  const { cards } = config.servicesSection;
   return (
     <Section>
-      <Card />
+      {cards.map((card, index) => (
+        <Card
+          key={index}
+          url={card.url}
+          label={card.label}
+          heading={card.heading}
+          description={card.description}
+        />
+      ))}
     </Section>
   );
 };
