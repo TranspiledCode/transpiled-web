f5077b0202169e5907b5418bd246b3220ebc914c
Joshua Crass <joshua.crass@gmail.com>
2024-11-12 15:52:05 -0800

    updated footer style (#28)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from '@emotion/styled';
+import GlobalContext from 'context/GlobalContext';
+import useScrollToTop from 'hooks/useScrollToTop';
+import Icon from 'components/Icon';
+
+const Footer = () => {
+  const { menuOpen } = useContext(GlobalContext);
+  const { isVisible, scrollToTop } = useScrollToTop(menuOpen, 20);
+
+  return (
+    <Background>
+      <ContentWrapper>
+        <CopyRight>
+          <Icon name="FaCopyright" size={1.5} /> Copyright 2024
+        </CopyRight>
+        <ScrollTop
+          aria-label="Scroll to top"
+          onClick={scrollToTop}
+          isVisible={isVisible}
+        >
+          Scroll to Top
+          <Icon name="FaArrowUp" size={1.5} />
+        </ScrollTop>
+      </ContentWrapper>
+    </Background>
+  );
+};
 
 const Background = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: ${({ theme }) => theme.colors.black};
-  height: 7rem;
+
   width: 100%;
+  height: 7rem;
   padding: 0 2rem;
+
+  font-family: ${({ theme }) => theme.fonts.manrope};
+  font-size: 1.8rem;
+
+  background-color: ${({ theme }) => theme.colors.black};
 
   @media (min-width: 768px) {
     padding: 0 6rem;
@@ -19,28 +50,27 @@ const ContentWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  max-width: ${({ theme }) => theme.layouts.maxWidth};
+
   width: 100%;
+  max-width: ${({ theme }) => theme.layouts.maxWidth};
 `;
+
 const CopyRight = styled.div`
-  font-family: Manrope;
-  font-size: 2rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
   color: ${({ theme }) => theme.colors.white};
 `;
+
 const ScrollTop = styled.div`
-  font-family: Manrope;
-  font-size: 2rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
   color: ${({ theme }) => theme.colors.white};
+
+  cursor: pointer;
 `;
-const Footer = () => {
-  return (
-    <Background>
-      <ContentWrapper>
-        <CopyRight>&#169; Copyright 2024</CopyRight>
-        <ScrollTop>Scroll to Top &#8593;</ScrollTop>
-      </ContentWrapper>
-    </Background>
-  );
-};
 
 export default Footer;
--- src/style/mediaQueries.js
+++ src/style/mediaQueries.js
@@ -1,6 +1,9 @@
 // mediaQueries.js
 const mediaQueries = {
+  sm: '@media (min-width: 640px)',
   md: '@media (min-width: 768px)',
+  lg: '@media (min-width: 1024px)',
+  xl: '@media (min-width: 1440px)',
 };
 
 export default mediaQueries;
