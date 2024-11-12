b258f4684453886164038fb876cfaa287a7880a1
Tyron Shi <tyronshi@gmail.com>
2024-11-12 12:48:48 -0500

    Style text

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,16 +2,22 @@ import styled from '@emotion/styled';
 
 const TestWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
+  gap: 2rem;
+
+  padding: 2rem;
+
   ${({ theme }) => theme.mediaQueries.md} {
-    gap: 2rem;
+    flex-direction: row;
+
     padding: 6rem;
   }
 `;
 
 const Container = styled.div`
-  height: 46rem;
-  width: 44rem;
+  height: 50rem;
+  width: 100%;
   border-image: linear-gradient(
       ${({ theme }) => theme.colors.lightBlue},
       ${({ theme }) => theme.colors.green}
@@ -19,8 +25,9 @@ const Container = styled.div`
     1;
   border-width: 4px;
   border-style: solid;
+  display: flex;
+  flex-direction: column;
 `;
-
 const Image = styled.div`
   background-image: url('https://images.unsplash.com/photo-1726137570078-1faa70beb6ad?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
   background-position: center;
@@ -29,11 +36,21 @@ const Image = styled.div`
   height: 70%;
   width: 100%;
 `;
+const TextContainer = styled.div`
+  padding: 2rem;
+`;
+
 const Heading = styled.h3`
-  font-family: ${({ theme }) => theme.fonts.poppins};
+  color: ${({ theme }) => theme.colors.black};
+  font-family: ${({ theme }) => theme.fonts.manrope};
+  font-size: 2.4rem;
+  letter-spacing: -0.4px;
 `;
 const Description = styled.p`
+  color: ${({ theme }) => theme.colors.darkGray};
   font-family: ${({ theme }) => theme.fonts.manrope};
+  font-size: 1.6rem;
+  text-align: justify;
 `;
 
 // const CardHeading = '';
@@ -44,27 +61,34 @@ const Card = () => {
     <TestWrapper>
       <Container>
         <Image></Image>
-        <Heading>Web Development</Heading>
-        <Description>
-          We build responsive, scalable websites with clean, future-proof code,
-          optimized for performance and built to grow with your business.
-        </Description>
+        <TextContainer>
+          <Heading>Web Development</Heading>
+          <Description>
+            We build responsive, scalable websites with clean, future-proof
+            code, optimized for performance and built to grow with your
+            business.
+          </Description>
+        </TextContainer>
       </Container>
       <Container>
         <Image></Image>
-        <Heading>Mobile App Development</Heading>
-        <Description>
-          Native and cross-platform mobile apps designed for performance and
-          user satisfaction, ensuring smooth navigation and fast load times.
-        </Description>
+        <TextContainer>
+          <Heading>Mobile App Development</Heading>
+          <Description>
+            Native and cross-platform mobile apps designed for performance and
+            user satisfaction, ensuring smooth navigation and fast load times.
+          </Description>
+        </TextContainer>
       </Container>
       <Container>
         <Image></Image>
-        <Heading>Custom Solutions</Heading>
-        <Description>
-          Optimized, secure e-commerce platforms designed to convert visitors
-          into customers while delivering a smooth shopping experience.
-        </Description>
+        <TextContainer>
+          <Heading>Custom Solutions</Heading>
+          <Description>
+            Optimized, secure e-commerce platforms designed to convert visitors
+            into customers while delivering a smooth shopping experience.
+          </Description>
+        </TextContainer>
       </Container>
     </TestWrapper>
   );
