6f4b571ef6f6dcf59d2420ab168d7f4eac704ec2
Tyron Shi <tyronshi@gmail.com>
2024-11-12 18:48:49 -0500

    Create services section and add to home.jsx

---
+++ src/components/ServicesSection.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+import Card from './Card';
+
+const Section = styled.div``;
+
+const ServicesSection = () => {
+  return (
+    <Section>
+      <Card />
+    </Section>
+  );
+};
+export default ServicesSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import Hero from 'components/Hero';
 import Footer from 'components/Footer';
 import SampleHeader from 'components/SampleHeader';
 import ContactSection from '../components/ContactSection';
+import ServicesSection from '../components/ServicesSection';
 
 const Home = () => {
   return (
     <>
       <SampleHeader />
       <Hero />
+      <ServicesSection />
       <ContactSection />
       <Footer />
     </>
