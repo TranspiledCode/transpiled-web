92417822b5ac12932f2d0f3461663fbb7a7ae049
Tyron Shi <tyronshi@gmail.com>
2024-11-12 12:12:00 -0500

    Temporarily add card component to home.jsx for previewing purposes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,18 @@ import Hero from 'components/Hero';
 import Footer from 'components/Footer';
 import SampleHeader from 'components/SampleHeader';
 import ContactSection from '../components/ContactSection';
+import Card from '../components/Card';
 
 const Home = () => {
   return (
     <>
       <SampleHeader />
       <Hero />
+
+      <>
+        <Card />
+      </>
+      
       <ContactSection />
       <Footer />
     </>
