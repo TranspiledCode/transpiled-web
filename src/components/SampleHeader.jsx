35ea7797f64a4057dfe758735a8a66c1daedbbd5
Tyron <65686818+TyronShi@users.noreply.github.com>
2024-11-13 17:59:20 -0500

    Mobile Nav Menu Button (feature/mobile-nav) (#37)
    * Intial component structure setup
    
    * Add mobile menu button component to sample header for previewing
    
    * Temporarily disable hover styles
    
    * Restore hover effects and clean up sample header for demo
    
    * Remove hover effects. Testing reveals visual issues with mobile devices.
    
    * Remove final hover effect
    
    * Add code optimization

--- src/components/SampleHeader.jsx
+++ src/components/SampleHeader.jsx
@@ -2,8 +2,8 @@ import { useEffect, useContext } from 'react';
 import styled from '@emotion/styled';
 
 import GlobalContext from '../context/GlobalContext';
-import Icon from './Icon';
 import NavBar from './NavBar';
+import MobileMenuButton from './MobileMenuButton';
 
 const SampleHeader = () => {
   const { scrolled, handleScroll } = useContext(GlobalContext);
@@ -24,13 +24,7 @@ const SampleHeader = () => {
       <Logo>Transpiled</Logo>
       <Nav>
         <NavBar links={links} />
-        <Hamburger>
-          <Icon
-            name={menuOpen ? 'FaTimes' : 'FaBars'}
-            size={1.8}
-            onClick={toggleMenu}
-          />
-        </Hamburger>
+        <MobileMenuButton onClick={toggleMenu} isOpen={menuOpen} />
       </Nav>
     </HeaderContainer>
   );
@@ -60,13 +54,6 @@ const Logo = styled.div`
 const Nav = styled.nav`
   display: flex;
   align-items: center;
-`;
-
-const Hamburger = styled.div`
-  font-size: 1.8rem;
-  @media (min-width: 768px) {
-    display: none;
-  }
 `;
 
 export default SampleHeader;
