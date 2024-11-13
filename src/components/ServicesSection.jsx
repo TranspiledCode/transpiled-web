385ee18a4b9b74b166418ee34775d92a41a7d0e5
Tyron Shi <tyronshi@gmail.com>
2024-11-12 19:52:47 -0500

    Added max width for large screens

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -2,14 +2,22 @@ import styled from '@emotion/styled';
 import Card from './Card';
 import config from '../config/home';
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const Section = styled.div`
+  max-width: 120rem;
+  width: 100%;
   display: grid;
   grid-template-columns: 1fr;
   gap: 2rem;
   padding: 2rem;
 
   ${({ theme }) => theme.mediaQueries.md} {
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(auto-fill, minmax(30%, 1fr));
     padding: 6rem;
   }
 `;
@@ -17,17 +25,19 @@ const Section = styled.div`
 const ServicesSection = () => {
   const { cards } = config.services;
   return (
-    <Section>
-      {cards.map((card, index) => (
-        <Card
-          key={index}
-          url={card.url}
-          label={card.label}
-          heading={card.heading}
-          description={card.description}
-        />
-      ))}
-    </Section>
+    <Container>
+      <Section>
+        {cards.map((card, index) => (
+          <Card
+            key={index}
+            url={card.url}
+            label={card.label}
+            heading={card.heading}
+            description={card.description}
+          />
+        ))}
+      </Section>
+    </Container>
   );
 };
 export default ServicesSection;
