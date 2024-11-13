982d73985ba49d1460401e4194b9ae22f4427dc9
Tyron <65686818+TyronShi@users.noreply.github.com>
2024-11-13 17:23:41 -0500

    Home Page Services Section (feature/services-section) (#32)
    * Create services section and add to home.jsx
    
    * Setup services section to have cards mapped
    
    * Card content added to home config
    
    * Added max width for large screens
    
    * Changed max-width to value found in theme file
    
    * Added call to action button
    
    * Add title area with section title and subtitle from config
    
    * Adjust subtitle width
    
    * Adjust card grid
    
    * Correct subtitle width with clamp to prevent excessive text shifting on different screen sizes
    
    * Card Responsiveness Fix (feature/card-update) (#35)
    
    * Fix card and section responsiveness to remove extra whitespace
    
    * Remove debug code from section
    
    * Remove unsued code from card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,6 @@ import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 
 const Container = styled.div`
-  height: 100%;
   width: 100%;
   border-image: linear-gradient(
       ${({ theme }) => theme.colors.lightBlue},
@@ -12,7 +11,7 @@ const Container = styled.div`
   border-width: 4px;
   border-style: solid;
   display: grid;
-  grid-template-rows: 1.5fr 1fr;
+  grid-auto-rows: max-content;
 `;
 const Image = styled.img`
   height: 100%;
@@ -20,15 +19,16 @@ const Image = styled.img`
   object-fit: cover;
 `;
 const TextContainer = styled.div`
-  padding: 2rem;
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
+  padding: 1.4rem 2rem 2rem;
 `;
 const Heading = styled.h3`
   color: ${({ theme }) => theme.colors.black};
   font-family: ${({ theme }) => theme.fonts.manrope};
-  font-size: 2.4rem;
+  font-size: clamp(1.8rem, 4vw, 2.4rem);
+  line-height: clamp(2rem, 4vw, 3rem);
   letter-spacing: -0.4px;
 `;
 const Description = styled.p`
---
+++ src/components/ServicesSection.jsx
@@ -0,0 +1,101 @@
+import styled from '@emotion/styled';
+import Card from './Card';
+import Button from './Button';
+import config from '../config/home';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  padding: 2rem;
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    gap: 4rem;
+    padding: 6rem;
+  }
+`;
+
+const SectionInfo = styled.div`
+  max-width: ${({ theme }) => theme.layouts.maxWidth};
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.5rem;
+`;
+const Title = styled.h2`
+  color: ${({ theme }) => theme.colors.lightBlue};
+  font-family: ${({ theme }) => theme.fonts.poppins};
+  font-weight: 700;
+  font-size: clamp(5rem, 8vw, 6.4rem);
+  line-height: clamp(5rem, 8vw, 6.4rem);
+  letter-spacing: -2px;
+`;
+const Subtitle = styled.p`
+  width: 100%;
+  color: ${({ theme }) => theme.colors.darkGray};
+  font-family: ${({ theme }) => theme.fonts.manrope};
+  font-weight: 400;
+  font-size: clamp(1.6rem, 4vw, 2.4rem);
+  text-align: justify;
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    width: clamp(60rem, 100%, 70rem);
+    text-align: left;
+  }
+`;
+
+const CardArea = styled.div`
+  max-width: ${({ theme }) => theme.layouts.maxWidth};
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    flex-direction: row;
+  }
+`;
+
+const ButtonArea = styled.div`
+  max-width: ${({ theme }) => theme.layouts.maxWidth};
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const ServicesSection = () => {
+  const { cards } = config.services;
+  return (
+    <Container>
+      <SectionInfo>
+        <Title>{config.services.title}</Title>
+        <Subtitle>{config.services.subtitle}</Subtitle>
+      </SectionInfo>
+      <CardArea>
+        {cards.map((card, index) => (
+          <Card
+            key={index}
+            url={card.url}
+            label={card.label}
+            heading={card.heading}
+            description={card.description}
+          />
+        ))}
+      </CardArea>
+      <ButtonArea>
+        <Button
+          type="call to action"
+          icon="FaArrowRight"
+          variant="ghost"
+          size="medium"
+        >
+          Explore Services
+        </Button>
+      </ButtonArea>
+    </Container>
+  );
+};
+export default ServicesSection;
