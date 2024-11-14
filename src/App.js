f225a8f21fdb03e4566915562ffae34728b9037c
Tyron <65686818+TyronShi@users.noreply.github.com>
2024-11-14 16:01:52 -0500

    Temporary Main Page (feature/main-placeholder) (#43)
    * Style new landing
    
    * Add additional styling
    
    * Define page height for mobile
    
    * Improve animation loop and make page body background dark

--- src/App.js
+++ src/App.js
@@ -3,29 +3,65 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  box-sizing: border-box;
+  display: grid;
+  place-content: center;
   height: 100vh;
-  background-color: #000;
+  height: 100dvh;
+  text-align: center;
+
+  color: #f8f8f8;
+
+  background-image: linear-gradient(to top, transparent, #214eea),
+    linear-gradient(to bottom, #15b5fa, #dd53ff, #15b5fa);
+  background-size:
+    100% 100%,
+    100% 400%;
+  animation: move 10s linear infinite alternate;
+
+  @keyframes move {
+    from {
+      background-position:
+        center center,
+        top center;
+    }
+    to {
+      background-position:
+        center center,
+        bottom center;
+    }
+  }
 `;
 
 const Title = styled.h1`
-  font-size: 48px;
-  color: #fff;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 700;
+  font-style: normal;
+  font-size: clamp(5.6rem, 7vw, 8rem);
+  letter-spacing: -2px;
 `;
 
 const Subtitle = styled.h3`
-  font-size: 24px;
-  color: #fff;
+  font-family: 'DM Mono', monospace;
+  font-weight: 400;
+  font-style: normal;
+  text-transform: uppercase;
+  font-size: clamp(1.2rem, 2vw, 1.8rem);
+  letter-spacing: 1px;
+`;
+
+const Highlight = styled.span`
+  color: #7fff43;
 `;
 
 const App = () => {
   return (
     <Container>
       <Title>Transpiled</Title>
-      <Subtitle>Transpiling Tech Complexity Into Simplicity</Subtitle>
+      <Subtitle>
+        Transpiling <Highlight>Complexity</Highlight> into{' '}
+        <Highlight>Clarity</Highlight>
+      </Subtitle>
     </Container>
   );
 };
