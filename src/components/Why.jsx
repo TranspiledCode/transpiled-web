c13f1da87d456db3f5e751f1d5a89a0922ac47eb
Dustin Reyes <Dustin@transpiled.com>
2024-11-14 18:02:18 -0700

    Started why page

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -1,54 +1,11 @@
-import React from 'react';
 import styled from '@emotion/styled';
 
-const backImage = styled.div``;
+const Wrapper = Styled.div``;
+const Title = styled.h2``;
+const SubTitle = styled.p``;
+const CardTitle = styled.h1``;
+const CardContent = styled.h1``;
 
-const title = styled.div`
-  display: flex;
-  text-align: left;
-  font-family: poppins;
-  font-size: 64px;
-  font-weight: bold;
-  color: #7fff43;
-  padding: 10px;
-`;
-const subject = styled.div`
-  display: flex;
-  text-align: left;
-  font-family: manrope;
-  font-size: 24px;
-  color: #f8f8f8;
-`;
-const BoxTitle = styled.div``;
-const BoxInfo = styled.div``;
-
-const LearnMore = styled.div``;
-
-<>
-  // imgsrc code below is incorrect //
-  <backImage imgsrc="#" alt="photo of highway"></backImage>
-  <title>Why Choose Us?</title>
-  <subject>Your Partner in Digital Innovation</subject>
-  <BoxTitle>Industry Expertise</BoxTitle>
-  <BoxInfo>
-    With a diverse portfolio, we bring specialized insights that help each
-    client acheive their unique goals
-  </BoxInfo>
-  <BoxTitle>Agile, Interative Process</BoxTitle>
-  <BoxInfo>
-    Our flexible, client-focused process keep projects efficient and aligned
-    with your evolving needs.
-  </BoxInfo>
-  <BoxTitle>Quality & Performance</BoxTitle>
-  <BoxInfo>
-    We build with the future in mind, using top tech and best practices for a
-    lasting, scalable solution
-  </BoxInfo>
-  <BoxTitle>Transparency</BoxTitle>
-  <BoxInfo>
-    From project updates to pricing, we ensure clarity, so you're always in the
-    loop-no hidden costs.
-  </BoxInfo>
-</>;
+<title>Why Choose Us?</title>;
 
 export default Why;
