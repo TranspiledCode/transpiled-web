9d7132dc3a2217cfab466a5b80e50fb77da76d5c
Tyron Shi <tyronshi@gmail.com>
2024-11-19 12:31:32 -0500

    Set up component structure

--- src/components/organisms/ServicesContact.jsx
+++ src/components/organisms/ServicesContact.jsx
@@ -1,4 +1,25 @@
+import styled from '@emotion/styled';
+import Button from '../atoms/Button';
+
+const SectionContainer = styled.section``;
+const ContentContainer = styled.div``;
+const Title = styled.h3``;
+const Subtitle = styled.p``;
+
 const ServicesContact = () => {
-  return;
+  return (
+    <SectionContainer>
+      <ContentContainer>
+        <Title>READY TO BUILD?</Title>
+        <Subtitle>
+          From websites and apps to custom solutions, we’re here to bring your
+          ideas to life. Let’s create something extraordinary together.
+        </Subtitle>
+        <Button icon="FaArrowRight" variant="outline" size="medium">
+          Contact Us
+        </Button>
+      </ContentContainer>
+    </SectionContainer>
+  );
 };
 export default ServicesContact;
