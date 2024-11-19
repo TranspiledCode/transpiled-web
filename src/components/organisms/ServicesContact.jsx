8d008195a110317f12e6bdb19f17ead6e271f0b9
Tyron Shi <tyronshi@gmail.com>
2024-11-19 12:49:57 -0500

    Add styling

--- src/components/organisms/ServicesContact.jsx
+++ src/components/organisms/ServicesContact.jsx
@@ -1,24 +1,69 @@
 import styled from '@emotion/styled';
 import Button from '../atoms/Button';
 
-const SectionContainer = styled.section``;
-const ContentContainer = styled.div``;
-const Title = styled.h3``;
-const Subtitle = styled.p``;
+const SectionContainer = styled.section`
+  width: 100%;
+  max-width: 100vw;
+  background: linear-gradient(
+    to bottom,
+    ${({ theme }) => theme.colors.fuchsia},
+    ${({ theme }) => theme.colors.darkBlue}
+  );
+  ${({ theme }) => theme.mixins.flexColCenter};
+  padding: ${({ theme }) => theme.layouts.sectionPadding};
+`;
+const SectionContent = styled.div`
+  width: 100%;
+  max-width: ${({ theme }) => theme.layouts.maxWidth};
+  padding: 2vw 0;
+`;
+const Title = styled.h3`
+  color: ${({ theme }) => theme.colors.white};
+  font-family: ${({ theme }) => theme.fonts.poppins};
+  font-weight: 700;
+  font-size: clamp(5rem, 8vw, 6.4rem);
+  line-height: 0.95em;
+  letter-spacing: -0.04em;
+`;
+
+const SubtitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: clamp(2rem, 4vw, 4rem);
+`;
+
+const Subtitle = styled.p`
+  width: 100%;
+  color: ${({ theme }) => theme.colors.white};
+  font-family: ${({ theme }) => theme.fonts.manrope};
+  font-weight: 400;
+  font-size: clamp(1.6rem, 4vw, 2.4rem);
+  text-align: justify;
+  line-height: 1.4em;
+  letter-spacing: -0.015em;
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    width: clamp(60rem, 100%, 90rem);
+    text-align: left;
+  }
+`;
 
 const ServicesContact = () => {
   return (
     <SectionContainer>
-      <ContentContainer>
+      <SectionContent>
         <Title>READY TO BUILD?</Title>
-        <Subtitle>
-          From websites and apps to custom solutions, we’re here to bring your
-          ideas to life. Let’s create something extraordinary together.
-        </Subtitle>
-        <Button icon="FaArrowRight" variant="outline" size="medium">
-          Contact Us
-        </Button>
-      </ContentContainer>
+        <SubtitleContainer>
+          <Subtitle>
+            From websites and apps to custom solutions, we’re here to bring your
+            ideas to life. Let’s create something extraordinary together.
+          </Subtitle>
+          <Button icon="FaArrowRight" variant="outline" size="medium">
+            Contact Us
+          </Button>
+        </SubtitleContainer>
+      </SectionContent>
     </SectionContainer>
   );
 };
