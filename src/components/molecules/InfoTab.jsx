f3383537a91c6b46ca6ca1cb7beeb693c49e13be
Tyron Shi <tyronshi@gmail.com>
2024-11-20 12:44:33 -0500

    Add initial styling

--- src/components/molecules/InfoTab.jsx
+++ src/components/molecules/InfoTab.jsx
@@ -1,12 +1,56 @@
 import styled from '@emotion/styled';
 
-const TabContainer = styled.div``;
-const Title = styled.h3``;
-const Subtitle = styled.p``;
-const FeatureArea = styled.div``;
-const Feature = styled.div``;
-const FeatureTitle = styled.h4``;
-const FeatureCaption = styled.p``;
+const TabContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Title = styled.h3`
+  color: ${({ theme }) => theme.colors.green};
+  font-family: ${({ theme }) => theme.fonts.poppins};
+  font-weight: 700;
+  font-size: clamp(5rem, 8vw, 6.4rem);
+  line-height: 0.95em;
+  letter-spacing: -0.04em;
+`;
+const Subtitle = styled.p`
+  color: ${({ theme }) => theme.colors.darkGray};
+  font-family: ${({ theme }) => theme.fonts.manrope};
+  font-weight: 400;
+  font-size: clamp(1.6rem, 4vw, 2.4rem);
+  text-align: justify;
+  line-height: 1.4em;
+  letter-spacing: -0.015em;
+`;
+const FeatureArea = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  padding: 2vw 0;
+`;
+const Feature = styled.div`
+  width: 30rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const FeatureTitle = styled.h4`
+  color: ${({ theme }) => theme.colors.black};
+  font-family: ${({ theme }) => theme.fonts.manrope};
+  font-weight: 700;
+  font-size: clamp(1.6rem, 4vw, 2.4rem);
+  text-align: justify;
+  line-height: 1.4em;
+  letter-spacing: -0.015em;
+`;
+const FeatureCaption = styled.p`
+  color: ${({ theme }) => theme.colors.darkGray};
+  font-family: ${({ theme }) => theme.fonts.manrope};
+  font-weight: 400;
+  font-size: clamp(1.6rem, 4vw, 2.4rem);
+  text-align: justify;
+  line-height: 1.4em;
+  letter-spacing: -0.015em;
+`;
 
 const InfoTab = () => {
   const title = 'WEB DEVELOPMENT';
