4c14c0c0dac3aaba3f6cf758db8811bf154bb9c7
Tyron Shi <tyronshi@gmail.com>
2024-11-20 12:25:25 -0500

    Component structure set up

--- src/components/molecules/InfoTab.jsx
+++ src/components/molecules/InfoTab.jsx
@@ -1,4 +1,25 @@
+import styled from '@emotion/styled';
+
+const TabContainer = styled.div``;
+const Title = styled.h3``;
+const Subtitle = styled.p``;
+const FeatureArea = styled.div``;
+const Feature = styled.div``;
+const FeatureTitle = styled.h4``;
+const FeatureCaption = styled.p``;
+
 const InfoTab = () => {
-  return;
+  return (
+    <TabContainer>
+      <Title></Title>
+      <Subtitle></Subtitle>
+      <FeatureArea>
+        <Feature>
+          <FeatureTitle></FeatureTitle>
+          <FeatureCaption></FeatureCaption>
+        </Feature>
+      </FeatureArea>
+    </TabContainer>
+  );
 };
 export default InfoTab;
