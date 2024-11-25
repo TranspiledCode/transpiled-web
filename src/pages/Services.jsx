f2c1e83c6fd99af47882238253d0d837ef6a98d8
Tyron Shi <tyronshi@gmail.com>
2024-11-24 21:31:37 -0500

    Temporarily remove component from services page for rebase.

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,7 +2,6 @@ import styled from '@emotion/styled';
 import SampleHeader from 'organisms/SampleHeader';
 import ServicesHero from '../components/organisms/ServicesHero';
 import ServicesContact from '../components/organisms/ServicesContact';
-import InfoTab from '../components/molecules/InfoTab';
 
 const ServicesWrapper = styled.section`
   display: flex;
@@ -11,32 +10,12 @@ const ServicesWrapper = styled.section`
   min-height: 100vh;
 `;
 
-// Remove After Debug //
-const TestWrapper = styled.div`
-  width: 100%;
-  max-width: 100vw;
-  ${({ theme }) => theme.mixins.flexColCenter};
-  padding: ${({ theme }) => theme.layouts.sectionPadding};
-`;
-
-const ContentWrapper = styled.div`
-  width: 100%;
-  max-width: ${({ theme }) => theme.layouts.maxWidth};
-  padding: 2vw 0;
-`;
-// Remove After Debug //
-
 const Services = () => {
   return (
     <ServicesWrapper>
       <SampleHeader />
       <ServicesHero />
       <ServicesContact />
-      <TestWrapper>
-        <ContentWrapper>
-          <InfoTab />
-        </ContentWrapper>
-      </TestWrapper>
     </ServicesWrapper>
   );
 };
