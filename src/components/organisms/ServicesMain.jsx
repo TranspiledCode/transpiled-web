84b2752b4c849008662946a98705e15fcd50e04c
Dustin Reyes <Dustin@Transpiled.com>
2024-11-25 20:13:02 -0700

    Started working on services page - Practice

---
+++ src/components/organisms/ServicesMain.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Header = styled.h1``;
+const SubHeader = styled.h2``;
+const Paragraph = styled.p``;
+const ServicesMain = () => {
+    const WhySection = () => {
+        return (
+            <>
+        <Header>TODAY'S SOLUTIONS FUTURE PROOFED</Header>   
+        <SubHeader>WHETHER YOU NEED A DYNAMIC WEBSITE, MOBILE APP, OR A CUSTOM-BUILT SYSTEM, TRANSPILED DELIVERS EXCEPTIONALLY DELIVERED RESULTS</SubHeader>
+        <Paragraph>EXPLORE OUR SERVICES IN DEPTH OR SCHEDULE A CONSULTATION BELOW</Paragraph>
+            </>
+        );
+      };
+export default ServicesMain;
