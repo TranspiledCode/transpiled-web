cca32229b63d228a577bd337765be37ebebb1731
Dustin Reyes <Dustin@Transpiled.com>
2024-11-29 20:51:00 -0700

    Updated services Page

---
+++ src/components/organisms/ServiceBuild.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Title = styled.h1``;
+const ServiceBuild = () => {
+  return <Title>Hello</Title>;
+};
+
+export default ServiceBuild;
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,11 +1,15 @@
 import SampleHeader from 'organisms/SampleHeader';
 import ServicesMain from 'organisms/ServicesMain';
+import ServiceBuild from 'organisms/ServiceBuild';
+import Footer from 'organisms/Footer';
 
 const ServicesPage = () => {
   return (
     <>
       <SampleHeader />
       <ServicesMain />
+      <ServiceBuild />
+      <Footer />
     </>
   );
 };
