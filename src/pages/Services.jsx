76f4548b823dc8242d90e4719b98aadb75197bd4
TyronShi <tyronshi@gmail.com>
2024-11-30 18:54:17 -0500

    Remove no longer needed styling

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,23 +1,14 @@
-import styled from '@emotion/styled';
 import ServicesHero from '../components/organisms/ServicesHero';
 import ServicesContact from '../components/organisms/ServicesContact';
 import ServicesInfo from '../components/organisms/ServicesInfo';
 
-const ServicesWrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  min-height: 100vh;
-`;
-
 const Services = () => {
   return (
-    <ServicesWrapper>
+    <>
       <ServicesHero />
       <ServicesInfo />
       <ServicesContact />
-    </ServicesWrapper>
+    </>
   );
 };
 
