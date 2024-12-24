2c98a4f1aacfe7fa9f274a3ccf024d018c02e1e9
Dustin Reyes <Dustin@Transpiled.com>
2024-12-23 20:12:11 -0700

    Attached About us link and added redirect page to app.js

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import AboutCard from 'molecules/AboutCard';
+// import AboutCard from 'molecules/AboutCard';
 
-const AboutWrapper = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  gap: 2rem;
-  padding: 2rem;
-  background-image: url('https://images.pexels.com/photos/2514035/pexels-photo-2514035.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
-  background-size: cover;
-  background-position: center;
-  z-index: 1;
+const AboutUsWrapper = styled.div`
+  min-height: 100vh;
+  max-width: 100vw;
+  background: linear-gradient(
+    to bottom,
+    ${({ theme }) => theme.colors.darkBlue},
+    ${({ theme }) => theme.colors.lightBlue}
+  );
 `;
+
+const StyledHeader = styled.h1``;
 
 const AboutUs = () => {
   return (
-    <AboutWrapper>
-      <AboutCard />
-    </AboutWrapper>
+    <AboutUsWrapper>
+      <StyledHeader>About Us</StyledHeader>
+    </AboutUsWrapper>
   );
 };
 
