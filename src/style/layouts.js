52f74c16d87e2b73ed17135489e2bc45b05f9703
TyronShi <tyronshi@gmail.com>
2024-12-24 15:52:37 -0500

    Update theme section padding to include responsive vertical padding in addition to existing horizontal padding.

--- src/style/layouts.js
+++ src/style/layouts.js
@@ -1,7 +1,7 @@
 // layouts.js
 const layouts = {
   maxWidth: '2000px',
-  sectionPadding: '4rem clamp(2rem, 6vw, 8rem)',
+  sectionPadding: 'clamp(2rem, 3vw, 4rem) clamp(2rem, 6vw, 8rem)',
   sectionPaddingNoTop: '0rem clamp(2rem, 6vw, 8rem)',
 };
 
