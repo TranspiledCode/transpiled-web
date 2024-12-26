6f0133ebc83b67df574c092b237249994fe1ea65
TyronShi <tyronshi@gmail.com>
2024-12-25 21:42:55 -0500

    Style mobile nav fullscreen view

--- src/components/organisms/MobileNavMenu.jsx
+++ src/components/organisms/MobileNavMenu.jsx
@@ -4,6 +4,35 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import GlobalContext from 'context/GlobalContext';
 
+const StyledMobileNav = styled.nav`
+  min-height: 100vh;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.black};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: ${({ theme }) => theme.zIndices.mobileNavOverlay};
+  transition:
+    opacity 0.3s ease,
+    visibility 0.3s ease;
+  opacity: ${({ ariaHidden }) => (ariaHidden ? 0 : 1)};
+  visibility: ${({ ariaHidden }) => (ariaHidden ? 'hidden' : 'visible')};
+`;
+
+const StyledLink = styled(Link)`
+  color: ${({ theme }) => theme.colors.white};
+  font-family: ${({ theme }) => theme.fonts.manrope};
+  font-weight: 400;
+  font-size: clamp(2rem, 2vw, 2.4rem);
+  letter-spacing: -0.015em;
+  position: relative;
+`;
+
 const MobileNavMenu = ({ links }) => {
   const { menuOpen, toggleMenu } = useContext(GlobalContext);
 
@@ -30,31 +59,3 @@ MobileNavMenu.propTypes = {
 };
 
 export default memo(MobileNavMenu);
-
-// Styled Components
-const StyledMobileNav = styled.nav`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 2rem;
-  background-color: ${({ theme }) => theme.colors.black};
-  height: 100vh;
-  width: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: ${({ theme }) => theme.zIndices.mobileNavOverlay};
-  transition:
-    opacity 0.3s ease,
-    visibility 0.3s ease;
-  opacity: ${({ ariaHidden }) => (ariaHidden ? 0 : 1)};
-  visibility: ${({ ariaHidden }) => (ariaHidden ? 'hidden' : 'visible')};
-`;
-
-const StyledLink = styled(Link)`
-  color: ${({ theme }) => theme.colors.white};
-  text-decoration: none;
-  position: relative;
-  font-size: 4rem;
-`;
