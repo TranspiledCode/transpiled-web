3b783c3a3839a4f6d5fc54cdb1a75a20a3f50bc1
Tyron <65686818+TyronShi@users.noreply.github.com>
2024-12-26 19:06:39 -0500

    Header/Nav Bar Fix (feature/nav-bar) (#81)
    * Add padding and max width from theme
    
    * Style logo
    
    * Style nav links
    
    * Fix background color syntax
    
    * Style mobile nav fullscreen view
    
    * Link logo to home

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useContext } from 'react';
-
+import { Link } from 'react-router-dom';
 import AuthContext from 'context/AuthContext';
 import styled from '@emotion/styled';
 
@@ -9,6 +9,46 @@ import MobileNavMenu from 'organisms/MobileNavMenu';
 import MobileMenuButton from 'atoms/MobileMenuButton';
 import links from 'data/navigation';
 import ProfileDropdown from 'organisms/ProfileDropdown';
+
+const HeaderContainer = styled.header`
+  width: 100%;
+  background-color: ${({ scrolled, theme }) =>
+    scrolled ? theme.colors.black : 'transparent'};
+  color: ${({ theme }) => theme.colors.white};
+  transition:
+    background-color 0.3s,
+    color 0.3s;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  z-index: ${({ theme }) => theme.zIndices.header};
+  padding: ${({ theme }) => theme.layouts.sectionPadding};
+  padding-top: 0rem;
+  padding-bottom: 0rem;
+`;
+
+const HeaderContent = styled.div`
+  height: 6rem;
+  width: 100%;
+  max-width: ${({ theme }) => theme.layouts.maxWidth};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Logo = styled.h1`
+  font-family: ${({ theme }) => theme.fonts.poppins};
+  font-weight: 700;
+  font-size: clamp(2.4rem, 3vw, 3.4rem);
+  letter-spacing: -0.04em;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+`;
 
 const SampleHeader = () => {
   const { scrolled, handleScroll } = useContext(GlobalContext);
@@ -22,43 +62,19 @@ const SampleHeader = () => {
 
   return (
     <HeaderContainer scrolled={scrolled}>
-      <MobileNavMenu links={links} />
-      <Logo>Transpiled</Logo>
-      <Nav>
-        <NavMenu links={links} />
-        <MobileMenuButton onClick={toggleMenu} isOpen={menuOpen} />
-        {currentUser && <ProfileDropdown />}
-      </Nav>
+      <HeaderContent>
+        <MobileNavMenu links={links} />
+        <Logo>
+          <Link to="/">Transpiled</Link>
+        </Logo>
+        <Nav>
+          <NavMenu links={links} />
+          <MobileMenuButton onClick={toggleMenu} isOpen={menuOpen} />
+          {currentUser && <ProfileDropdown />}
+        </Nav>
+      </HeaderContent>
     </HeaderContainer>
   );
 };
-
-const HeaderContainer = styled.header`
-  position: fixed;
-  top: 0;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 1rem 2rem;
-  background-color: ${({ scrolled }) =>
-    scrolled ? '${({ theme }) => theme.colors.black}' : 'transparent'};
-  color: ${({ theme }) => theme.colors.white};
-  transition:
-    background-color 0.3s,
-    color 0.3s;
-  z-index: ${({ theme }) => theme.zIndices.header};
-`;
-
-const Logo = styled.div`
-  font-size: 2rem;
-  font-weight: bold;
-`;
-
-const Nav = styled.nav`
-  display: flex;
-  align-items: center;
-  gap: 2rem;
-`;
 
 export default SampleHeader;
