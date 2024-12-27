7e9d3b37d101b01333b6a18430442e93deaf9477
Dustin Reyes <Dustin@Transpiled.com>
2024-12-26 20:52:46 -0700

    Updated padding on bottom of Why page

--- src/components/organisms/WhySection.jsx
+++ src/components/organisms/WhySection.jsx
@@ -10,7 +10,7 @@ const Container = styled.div`
   align-items: flex-start;
   justify-content: center;
   gap: 2rem;
-  padding: 2rem;
+  padding: 2rem 2rem 5rem;
   background-image: url('https://images.pexels.com/photos/2514035/pexels-photo-2514035.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
   background-size: cover;
   background-position: center;
