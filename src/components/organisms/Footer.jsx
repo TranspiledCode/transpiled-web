7e25ab4aae936443cb6ca1ae85c0c926ade66b9e
Tyron <65686818+TyronShi@users.noreply.github.com>
2024-12-26 19:06:54 -0500

    Style mini footer (#82)

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -6,6 +6,35 @@ import ContactInfo from 'organisms/ContactInfo';
 
 import MiniFooter from 'molecules/MiniFooter';
 
+const BaseFooter = styled.div`
+  width: 100%;
+  ${({ theme }) => theme.mixins.flexColCenter};
+  background-color: ${({ theme }) => theme.colors.black};
+  font-family: ${({ theme }) => theme.fonts.manrope};
+  font-weight: 400;
+  font-size: 1.6rem;
+  letter-spacing: -0.01em;
+  z-index: ${({ theme }) => theme.zIndices.footer};
+  padding: ${({ theme }) => theme.layouts.sectionPadding};
+`;
+
+const MiniFooterWrapper = styled(BaseFooter)`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  padding-top: 0;
+  padding-bottom: 0;
+  height: 4rem;
+`;
+
+const MainFooter = styled(BaseFooter)`
+  position: relative;
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    height: 17rem;
+  }
+`;
+
 const Footer = () => {
   const isAtBottom = useIsAtBottom(16);
 
@@ -23,31 +52,4 @@ const Footer = () => {
     </>
   );
 };
-
-// BaseFooter styles
-const BaseFooter = styled.div`
-  width: 100%;
-  ${({ theme }) => theme.mixins.flexColCenter};
-  padding: ${({ theme }) => theme.layouts.sectionPadding};
-  background-color: ${({ theme }) => theme.colors.black};
-  font-family: ${({ theme }) => theme.fonts.manrope};
-  font-size: clamp(1.6rem, 2vw, 1.8rem);
-  z-index: ${({ theme }) => theme.zIndices.footer};
-`;
-
-const MiniFooterWrapper = styled(BaseFooter)`
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  height: 6rem;
-`;
-
-const MainFooter = styled(BaseFooter)`
-  position: relative;
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    height: 17rem;
-  }
-`;
-
 export default Footer;
