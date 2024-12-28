b3472ba2bed00f2ef12e9755657d298656897f08
TyronShi <tyronshi@gmail.com>
2024-12-28 11:53:33 -0500

    Style component

--- src/components/molecules/TitleSubtitle.jsx
+++ src/components/molecules/TitleSubtitle.jsx
@@ -1,6 +1,47 @@
-// import styled from '@emotion/styled';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
 
-const TitleSubtitle = () => {
-  return;
+const Container = styled.div`
+  max-width: ${({ theme }) => theme.layouts.maxWidth};
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.5rem;
+`;
+const Title = styled.h2`
+  color: ${({ theme }) => theme.colors.lightBlue};
+  font-family: ${({ theme }) => theme.fonts.poppins};
+  font-weight: 700;
+  font-size: clamp(4.8rem, 8vw, 6.4rem);
+  line-height: 0.95em;
+  letter-spacing: -0.04em;
+`;
+const Subtitle = styled.p`
+  width: 100%;
+  color: ${({ theme }) => theme.colors.darkGray};
+  font-family: ${({ theme }) => theme.fonts.manrope};
+  font-weight: 400;
+  font-size: clamp(1.6rem, 4vw, 2.4rem);
+  text-align: justify;
+  line-height: 1.4em;
+  letter-spacing: -0.01em;
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    width: clamp(60rem, 100%, 70rem);
+    text-align: left;
+  }
+`;
+const TitleSubtitle = ({ title, subtitle }) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Subtitle>{subtitle}</Subtitle>
+    </Container>
+  );
+};
+TitleSubtitle.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
 };
 export default TitleSubtitle;
