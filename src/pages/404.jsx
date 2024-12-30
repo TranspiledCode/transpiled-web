8264ffdb6e68528d6fac85a7a691ef8e69783029
TyronShi <tyronshi@gmail.com>
2024-12-30 13:02:27 -0500

    Style 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,19 +1,49 @@
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+import Button from '../components/atoms/Button';
 
-const ServicesWrapper = styled.section`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+const PageWrapper = styled.section`
+  ${({ theme }) => theme.mixins.flexColCenter};
   min-height: 100vh;
-  background-color: ${({ theme }) => theme.colors.darkBlue};
+  background-color: ${({ theme }) => theme.colors.black};
+  gap: 4rem;
+`;
+const TitleWrapper = styled.div`
+  ${({ theme }) => theme.mixins.flexColCenter};
+`;
+const Title = styled.h1`
+  font-family: ${({ theme }) => theme.fonts.poppins};
   color: ${({ theme }) => theme.colors.white};
+  font-weight: 700;
+  font-size: 8rem;
+  line-height: 0.95em;
+  letter-spacing: -0.04em;
+  text-transform: uppercase;
+`;
+const Subtitle = styled.p`
+  font-family: ${({ theme }) => theme.fonts.mono};
+  color: ${({ theme }) => theme.colors.white};
+  font-weight: 400;
+  font-size: 2rem;
+  text-align: justify;
+  line-height: 1.4em;
+  letter-spacing: -0.01em;
+  text-transform: uppercase;
 `;
 
 const Services = () => {
   return (
-    <ServicesWrapper>
-      <h1>404 - Page Not Found</h1>
-    </ServicesWrapper>
+    <PageWrapper>
+      <TitleWrapper>
+        <Title>404</Title>
+        <Subtitle>Page Not Found</Subtitle>
+      </TitleWrapper>
+      <Link to="/">
+        <Button variant="outline" size="medium" icon="FaHome">
+          Return to Home
+        </Button>
+      </Link>
+    </PageWrapper>
   );
 };
 
