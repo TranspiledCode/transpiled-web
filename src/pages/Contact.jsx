e6a42dd486148662790a55ecf9fdc063ea298048
TyronShi <tyronshi@gmail.com>
2024-12-30 11:43:48 -0500

    intial page setup and baked in temporary content added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
 import ContactSection from '../components/organisms/ContactSection';
+import PageHero from '../components/organisms/PageHero';
+import PageCTA from '../components/organisms/PageCTA';
 
 const PageWrapper = styled.div`
   min-height: 100vh;
@@ -12,7 +14,22 @@ const PageWrapper = styled.div`
 const ContactPage = () => {
   return (
     <PageWrapper>
+      <PageHero
+        gradTopCol="darkBlue"
+        gradBotCol="fuchsia"
+        title="Build your future here, today"
+        subtitle="Whether you need a website refresh or something totally custom, we're here to make it happen."
+        caption="Ask about your upcoming project or schedule a consultation below."
+      />
       <ContactSection />
+      <PageCTA
+        gradTopCol="lightBlue"
+        gradBotCol="darkBlue"
+        title="Still on the Fence?"
+        subtitle="Feel free to meet our team, and learn more about how we approach every project with creativity, dedication and care."
+        btnText="About Us"
+        btnUrl="/about"
+      />
     </PageWrapper>
   );
 };
