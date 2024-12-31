d3fc7f166bd18af35799d051bc4fda7d93b5f131
TyronShi <tyronshi@gmail.com>
2024-12-31 18:03:11 -0500

    Add temporary content to page

--- src/components/organisms/PortfolioList.jsx
+++ src/components/organisms/PortfolioList.jsx
@@ -1,4 +1,23 @@
+import styled from '@emotion/styled';
+import TitleSubtitle from '../molecules/TitleSubtitle';
+
+const SectionContainer = styled.section`
+  padding: ${({ theme }) => theme.layouts.sectionPadding};
+  ${({ theme }) => theme.mixins.flexColCenter}
+`;
+const SectionContent = styled.div`
+  min-width: 100%;
+  max-width: ${({ theme }) => theme.layouts.maxWidth};
+  ${({ theme }) => theme.mixins.flexColCenter}
+`;
+
 const PortfolioList = () => {
-  return;
+  return (
+    <SectionContainer>
+      <SectionContent>
+        <TitleSubtitle />
+      </SectionContent>
+    </SectionContainer>
+  );
 };
 export default PortfolioList;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,9 +5,22 @@ import PageCTA from '../components/organisms/PageCTA';
 const Portfolio = () => {
   return (
     <>
-      <PageHero />
+      <PageHero
+        gradTopCol="fuchsia"
+        gradBotCol="orange"
+        title="Work that speaks for itself"
+        subtitle="From responsive designs to user-friendly interfaces, our work reflects our commitment to quality and innovation."
+        caption="Take a look at our past projects below"
+        stMaxWidth={70}
+      />
       <PortfolioList />
-      <PageCTA />
+      <PageCTA
+        gradTopCol="lightBlue"
+        gradBotCol="green"
+        title="Want Something Else?"
+        subtitle="Whether you need a dynamic website, a mobile app, or a custom-built system, Transpiled delivers exceptional results."
+        btnText="Explore Services"
+      />
     </>
   );
 };
