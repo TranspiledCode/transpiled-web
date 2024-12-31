7418a0d7a4acefee28baa59f4577f1dfc239804c
TyronShi <tyronshi@gmail.com>
2024-12-31 17:52:47 -0500

    Create portfolio list component

---
+++ src/components/organisms/PortfolioList.jsx
@@ -0,0 +1,4 @@
+const PortfolioList = () => {
+  return;
+};
+export default PortfolioList;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,12 @@
-import PageCTA from '../components/organisms/PageCTA';
 import PageHero from '../components/organisms/PageHero';
+import PortfolioList from '../components/organisms/PortfolioList';
+import PageCTA from '../components/organisms/PageCTA';
 
 const Portfolio = () => {
   return (
     <>
       <PageHero />
+      <PortfolioList />
       <PageCTA />
     </>
   );
