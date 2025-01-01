9266979e4c9a8666e6bb8fa0153fbccacfc61988
TyronShi <tyronshi@gmail.com>
2024-12-31 20:39:52 -0500

    Refine image component inputs

--- src/components/atoms/Image.jsx
+++ src/components/atoms/Image.jsx
@@ -2,20 +2,34 @@ import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 
 const StyledImage = styled.img`
-  position: absolute;
+  position: ${({ position }) => position};
   width: 100%;
   height: 100%;
-  object-fit: cover;
+  object-fit: ${({ objFit }) => objFit};
   z-index: -2;
 `;
 
-const Image = ({ url = '', label = 'image' }) => {
+const Image = ({
+  url,
+  label = 'image',
+  objFit = 'cover',
+  position = 'static',
+}) => {
   return (
-    <StyledImage src={url} alt={label} aria-label={label} loading="lazy" />
+    <StyledImage
+      src={url}
+      alt={label}
+      aria-label={label}
+      loading="lazy"
+      objFit={objFit}
+      position={position}
+    />
   );
 };
 export default Image;
 Image.propTypes = {
   url: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
+  objFit: PropTypes.oneOf(['cover', 'contain']),
+  position: PropTypes.oneOf(['relative', 'absolute', 'static']),
 };
