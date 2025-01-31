a4342abf902105ea5c2aa10f614cc93895699d97
Tyron Shi <tyronshi@gmail.com>
2025-01-30 22:29:04 -0500

    cleanup code

--- src/hooks/useNoScroll.js
+++ src/hooks/useNoScroll.js
@@ -1,19 +1,17 @@
 import { useEffect } from 'react';
 
-// Custom hook to lock/unlock scroll on the body
 const useNoScroll = (isMenuOpen) => {
   useEffect(() => {
     if (isMenuOpen) {
-      document.body.style.overflow = 'hidden'; // Disable scroll
+      document.body.style.overflow = 'hidden';
     } else {
-      document.body.style.overflow = 'auto'; // Enable scroll
+      document.body.style.overflow = 'auto';
     }
 
-    // Cleanup to ensure scroll is restored when component unmounts
     return () => {
-      document.body.style.overflow = 'auto'; // Reset to default
+      document.body.style.overflow = 'auto';
     };
-  }, [isMenuOpen]); // Effect runs whenever the menu open state changes
+  }, [isMenuOpen]);
 };
 
 export default useNoScroll;
